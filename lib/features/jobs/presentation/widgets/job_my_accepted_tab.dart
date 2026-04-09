72e47db5ce0d34586bec523d3ecb3529926b8b63
RujakBuah <farizm899@gmail.com>
2026-04-09 15:50:52 +0900

    Finalized Main Content

--- lib/features/jobs/presentation/widgets/job_my_accepted_tab.dart
+++ lib/features/jobs/presentation/widgets/job_my_accepted_tab.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pt_best/core/common/entities/job.dart';
+import 'package:pt_best/core/constants/constants.dart';
+import 'package:pt_best/core/theme/app_palete.dart';
+import 'package:pt_best/core/utils/capitilize_first_letter.dart';
+import 'package:pt_best/core/utils/format_number.dart';
+import 'package:pt_best/features/jobs/presentation/widgets/job_info_apply_button.dart';
 
 class AcceptedJobsTab extends StatelessWidget {
   final List<Job> jobs;
@@ -7,6 +12,10 @@ class AcceptedJobsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void markAsComplete() {
+      print('Marking job as complete');
+    }
+
     if (jobs.isEmpty) {
       return const Center(child: Text('No accepted jobs'));
     }
@@ -14,7 +23,169 @@ class AcceptedJobsTab extends StatelessWidget {
       itemCount: jobs.length,
       itemBuilder: (context, index) {
         final job = jobs[index];
-        return Text(job.title);
+        return Container(
+          padding: EdgeInsets.all(16),
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: AppPalette.surface,
+            border: Border.all(color: AppPalette.border),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Constants.jobTypeBgColors[job.jobType],
+                    ),
+                    child: Icon(
+                      Constants.jobTypeIcons[job.jobType],
+                      color: Constants.jobTypeIconColors[job.jobType],
+                      size: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  RichText(
+                    text: TextSpan(
+                      text: job.companyName,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: AppPalette.textPrimary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: '\n${job.title}',
+                          style: Theme.of(context).textTheme.bodyMedium!
+                              .copyWith(
+                                color: AppPalette.textSecondary,
+                                fontWeight: FontWeight.normal,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: Constants.jobStatusBgColors[job.status],
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(
+                      child: RichText(
+                        text: TextSpan(
+                          text: capitilize(job.status),
+                          style: Theme.of(context).textTheme.bodyLarge!
+                              .copyWith(
+                                color: AppPalette.textOnDark,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              RichText(
+                text: TextSpan(
+                  text: formatAsCurrency(job.payout),
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                    color: AppPalette.textPrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'In Progress',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: AppPalette.textSecondary,
+                        ),
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: '60%',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: AppPalette.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsGeometry.symmetric(horizontal: 8),
+                child: LinearProgressIndicator(
+                  value: 0.6,
+                  backgroundColor: AppPalette.border,
+                ),
+              ),
+              Divider(),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: 'POSTED BY\n',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                        color: AppPalette.iconDefault,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: job.companyName,
+                          style: Theme.of(context).textTheme.labelLarge!
+                              .copyWith(color: AppPalette.textPrimary),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  RichText(
+                    text: TextSpan(
+                      text: 'COMPLETE BY\n',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                        color: AppPalette.iconDefault,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: formatAsDate(job.completeBy),
+                          style: Theme.of(context).textTheme.labelLarge!
+                              .copyWith(color: AppPalette.textPrimary),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 8,
+                  ),
+                  child: JobInfoApplyButton(
+                    buttonText: 'Mark as Complete',
+                    onPressed: markAsComplete,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
